b20e10a61aefddda2acda8e6d2bbb2b9be260b51
Logan Moore <lmooore@gmail.com>
2015-12-13 13:04:33 -0500

    Added escape codes to quit program
    Also added debug line at the bottom of the screen to show the last pressed key

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,90 @@
+// Rusman - Rust + ncurses-based console game
+// Copyright (C) 2015 Logan Moore
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#![feature(convert)]
+
 extern crate ncurses;
 
 use ncurses::*;
+use std::fmt;
 
 static WINDOW_WIDTH: i32 = 32;
 static WINDOW_HEIGHT: i32 = 20;
+const ESC: i32 = 0x1B;
+const CTRL_C: i32= 0x3;
 
-/// Console-based pacman
+/// Rust + ncurses-based console game
 fn main() {
     // Initialize `ncurses`
     initscr();
+    raw();
+    keypad(stdscr, true);
+    noecho();
+    start_color();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+
+    let (max_y, max_x) = get_max_bounds(stdscr);
 
     let window = create_window(0, 0);
 
     let mut ch = getch();
-    while ch != KEY_F(4) {
-        
+
+    // Main user input loop
+    loop {
+        mvprintw(LINES - 1, 0, format!("{}, {}", ch, keyname(ch)).as_str());
+        match ch {
+            // Player movement
+            KEY_LEFT => {
+                // TODO
+            },
+
+            KEY_RIGHT => {
+                // TODO
+            },
+
+            KEY_UP => {
+                // TODO
+            },
+
+            KEY_DOWN => {
+                // TODO
+            },
+
+            ERR => {
+                printw("ERR");
+            },
+
+            ESC => {
+                printw("Found escape");
+                break
+            },
+
+            KEY_F4 => {
+                printw("F4 Pressed");
+                break
+            },
+
+            KEY_CLOSE => {
+                break
+            }
+
+            CTRL_C => {
+                break
+            }
+
+            _ => {
+            },
+        }
         ch = getch();
     }
 
@@ -23,6 +93,7 @@ fn main() {
     endwin();
 }
 
+/// Open a new, boxed ncurses window
 fn create_window(y: i32, x: i32) -> WINDOW {
     let window = newwin(WINDOW_WIDTH, WINDOW_HEIGHT, y, x);
     box_(window, 0, 0);
@@ -30,9 +101,17 @@ fn create_window(y: i32, x: i32) -> WINDOW {
     window
 }
 
+/// Destroy an ncurses window
 fn destroy_window(window: WINDOW) {
     let ch = ' ' as chtype;
     wborder(window, ch, ch, ch, ch, ch, ch, ch, ch);
     wrefresh(window);
     delwin(window);
+}
+
+/// Get the maximum bounds of the console
+fn get_max_bounds(window: WINDOW) -> (i32, i32) {
+    let (mut max_y, mut max_x) = (0, 0);
+    getmaxyx(window, &mut max_y, &mut max_x);
+    (max_y, max_x)
 }
